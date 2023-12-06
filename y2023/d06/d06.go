6eb0a7c95a2c78b8c8754846089fe67b529e00e6
Jon Erickson <erjonath@twitch.tv>
2023-12-05 21:39:13 -0800

    day 6 tests and input parsing

--- y2023/d06/d06.go
+++ y2023/d06/d06.go
@@ -1,16 +1,118 @@
 package d06
 
-func WaysToWin(time, distance int) int {
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"errors"
+	"strconv"
+)
+
+//go:embed input.txt
+var input []byte
+
+func ReadInput() []string {
+	s := bufio.NewScanner(bytes.NewBuffer(input))
+	var out []string
+	for s.Scan() {
+		out = append(out, s.Text())
+	}
+	return out
+}
+
+type Race struct {
+	Time     int
+	Distance int
+}
+
+func ParseInputPart1(data []string) ([]Race, error) {
+	if len(data) != 2 {
+		return nil, errors.New("invalid input")
+	}
+
+	times, err := ParseNumbers(data[0])
+	if err != nil {
+		return nil, err
+	}
+
+	distances, err := ParseNumbers(data[1])
+	if err != nil {
+		return nil, err
+	}
+
+	if len(times) != len(distances) {
+		return nil, errors.New("invalid input")
+	}
+
+	out := make([]Race, len(times))
+	for i := 0; i < len(times); i++ {
+		out[i] = Race{
+			Time:     times[i],
+			Distance: distances[i],
+		}
+	}
+	return out, nil
+}
+
+func ParseNumbers(data string) ([]int, error) {
+	var out []int
+	for i := 0; i < len(data); i++ {
+		var num []byte
+		for i < len(data) && '0' <= data[i] && data[i] <= '9' {
+			num = append(num, data[i])
+			i++
+		}
+		if len(num) > 0 {
+			n, err := strconv.Atoi(string(num))
+			if err != nil {
+				return nil, err
+			}
+			out = append(out, n)
+		}
+	}
+	return out, nil
+}
+
+func ParseInputPart2(data []string) (Race, error) {
+	if len(data) != 2 {
+		return Race{}, errors.New("invalid input")
+	}
+
+	time, err := ParseNumber(data[0])
+	if err != nil {
+		return Race{}, err
+	}
+
+	distance, err := ParseNumber(data[1])
+	if err != nil {
+		return Race{}, err
+	}
+
+	return Race{
+		Time:     time,
+		Distance: distance,
+	}, nil
+}
+
+func ParseNumber(data string) (int, error) {
+	var num []byte
+	for i := 0; i < len(data); i++ {
+		if '0' <= data[i] && data[i] <= '9' {
+			num = append(num, data[i])
+		}
+	}
+	return strconv.Atoi(string(num))
+}
+
+func WaysToWin(race Race) int {
 	var speed int
 	var wins int
-
-	for i := 0; i <= time; i++ {
-		timeRemaining := time - i
-		if speed*timeRemaining > distance {
+	for i := 0; i <= race.Time; i++ {
+		timeRemaining := race.Time - i
+		if speed*timeRemaining > race.Distance {
 			wins++
 		}
 		speed++
 	}
-
 	return wins
 }
--- y2023/d06/d06_test.go
+++ y2023/d06/d06_test.go
@@ -4,25 +4,67 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Solve(t *testing.T) {
 	t.Run("Part 1", func(t *testing.T) {
-		ans := WaysToWin(38, 234)
-		ans *= WaysToWin(67, 1027)
-		ans *= WaysToWin(76, 1157)
-		ans *= WaysToWin(73, 1236)
+		races, err := ParseInputPart1(ReadInput())
+		require.NoError(t, err)
+
+		var ans int
+		for i, race := range races {
+			if i == 0 {
+				ans = WaysToWin(race)
+			} else {
+				ans *= WaysToWin(race)
+			}
+		}
 		assert.Equal(t, 303_600, ans)
 	})
 
 	t.Run("Part 2", func(t *testing.T) {
-		ans := WaysToWin(38_67_76_73, 234_1027_1157_1236)
-		assert.Equal(t, 23_654_842, ans)
+		race, err := ParseInputPart2(ReadInput())
+		require.NoError(t, err)
+		assert.Equal(t, 23_654_842, WaysToWin(race))
 	})
 }
 
 func Test_WaysToWin(t *testing.T) {
-	assert.Equal(t, 4, WaysToWin(7, 9))
-	assert.Equal(t, 8, WaysToWin(15, 40))
-	assert.Equal(t, 9, WaysToWin(30, 200))
+	races, err := ParseInputPart1([]string{
+		"Time:      7  15   30",
+		"Distance:  9  40  200",
+	})
+	require.NoError(t, err)
+
+	require.Len(t, races, 3)
+	assert.Equal(t, Race{
+		Time:     7,
+		Distance: 9,
+	}, races[0])
+	assert.Equal(t, 4, WaysToWin(races[0]))
+	assert.Equal(t, 8, WaysToWin(races[1]))
+	assert.Equal(t, 9, WaysToWin(races[2]))
+}
+
+func Test_ParseInput(t *testing.T) {
+	t.Run("Part 1", func(t *testing.T) {
+		races, err := ParseInputPart1(ReadInput())
+		require.NoError(t, err)
+		assert.Equal(t, []Race{
+			{Time: 38, Distance: 234},
+			{Time: 67, Distance: 1027},
+			{Time: 76, Distance: 1157},
+			{Time: 73, Distance: 1236},
+		}, races)
+	})
+
+	t.Run("Part 2", func(t *testing.T) {
+		race, err := ParseInputPart2(ReadInput())
+		require.NoError(t, err)
+		assert.Equal(t, Race{
+			Time:     38_67_76_73,
+			Distance: 234_1027_1157_1236,
+		}, race)
+	})
 }
